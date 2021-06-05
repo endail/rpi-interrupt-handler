72a916d5647505520b24ea60cbe2d4aed04dbe4f
endail <52652357+endail@users.noreply.github.com>
2021-06-05 23:05:41 +1000

    add test

--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,31 @@
 
 #include "../include/InterruptHandler.h"
+#include <functional>
+#include <iostream>
+#include <thread>
 
+using namespace std;
 using namespace InterruptHandler;
+
+void onInterrupt() {
+    std::cout << "***interrupt***" << std::endl;
+}
 
 int main() {
 
-    
+    int gpioPin = 8;
+    Edge edgeType = Edge::FALLING;
+    std::function<void()> cb(&onInterrupt);
 
+    InterruptHandler::init();
+    InterruptHandler::attachInterrupt(
+        gpioPin,
+        edgeType,
+        cb);
 
+    while(true) {
+        std::this_thread::yield();
+    }
 
     return 0;
 
